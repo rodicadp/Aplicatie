3bbce2d6e95266d230ab3f32ca14e79418b793f3
rodicadp <56158034+rodicadp@users.noreply.github.com>
2020-05-13 01:15:58 +0300

    Create tabs_debate.js

---
+++ js/tabs_debate.js
@@ -0,0 +1,19 @@
+/*code - we3 school */
+function openTab(evt, artType)
+{
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++)
+      {
+        tabcontent[i].style.display = "none";
+      }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++)
+      {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+  document.getElementById(artType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
