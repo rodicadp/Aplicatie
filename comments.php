71c0b40b45c51af58938615cdeb882ee53268db1
rodicadp <56158034+rodicadp@users.noreply.github.com>
2020-05-30 17:10:55 +0300

    Fix: SQL query.

--- comments.php
+++ comments.php
@@ -3,13 +3,14 @@ if(isset($_POST['user_comm']))
 {
   $id_debate =$_POST['id_debate'];
   $comment=$_POST['user_comm'];
-  #$id_user = $_SESSION['id_user'];
-  $insert=mysqli_query("INSERT INTO pro (id_debate, id_user, comment, createdOn)
-   VALUES ('$id_debate','".$_SESSION['id_user']."','$comment', NOW())");
+  $id_user = $_SESSION['id_user'];
+  $insert_query="INSERT INTO pro (id_debate, id_user, comment, createdOn)
+   VALUES ('$id_debate','$id_user',$comment', NOW())";
 
-  $id=mysql_insert_id($insert);
+  $insert = mysqli_query($conexiune,$insert_query);
+  $id=mysqli_insert_id($conexiune);
 
-  $select=mysqli_query("SELECT username,comment,DATE_FORMAT(pro.createdOn, '%Y-%m-%d') AS createdOn from pro join users where users.id_user='$id_user' and comment='$comment' and id_pro='$id'");
+  $select=mysqli_query($conexiune,"SELECT username,comment,DATE_FORMAT(pro.createdOn, '%Y-%m-%d') AS createdOn from pro join users where users.id_user='$id_user' and comment='$comment' and id_pro='$id'");
 
   if($row=mysqli_fetch_array($select))
   {
@@ -18,10 +19,10 @@ if(isset($_POST['user_comm']))
       $time=$row['createdOn'];
   ?>
      <div class="comment_div">
- 	    <p class="name">Posted by: <?php echo $name;?></p>
-         <p class="comment"><?php echo $comment;?></p>
- 	    <p class="time"><?php echo $time;?></p>
- 	  </div>
+	    <p class="name">Posted by: <?php echo $name;?></p>
+        <p class="comment"><?php echo $comment;?></p>
+	    <p class="time"><?php echo $time;?></p>
+    </div>
   <?php
   }
 exit;
